21366ca3bb2dfede27a263f7fe1354aa997b79ec
d1r0l <xd1r0lx@gmail.com>
2023-06-17 20:04:35 +0400

    Separated App module

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,32 +1,5 @@
-const { MONGODB_URI, PORT } = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
-
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const { PORT } = require('./utils/config')
+const app = require ('./app')
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
