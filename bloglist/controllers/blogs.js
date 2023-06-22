a18bcf97d7c83d5c10ffa73ec6f5fd213080166c
d1r0l <xd1r0lx@gmail.com>
2023-06-22 23:00:39 +0400

    small refactor

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -7,16 +7,17 @@ blogsRouter.get('/', (request, response) => {
   })
 })
 
-blogsRouter.post('/', async (request, response, next) => {
-  try {
-    const blog = new Blog(request.body)
-    await blog.save().then((result) => {
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+  blog
+    .save()
+    .then((result) => {
       response.status(201).json(result)
     })
-  } catch (exeption) {
-    response.status(400).end()
-    next(exeption)
-  }
+    .catch((exeption) => {
+      response.status(400).end()
+      next(exeption)
+    })
 })
 
 module.exports = blogsRouter
