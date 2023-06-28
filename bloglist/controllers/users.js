af358b135e6a971083ae4bded601005d4536fa7f
d1r0l <xd1r0lx@gmail.com>
2023-06-28 21:33:35 +0400

    added a verification for ex 4.16

--- bloglist/controllers/users.js
+++ bloglist/controllers/users.js
@@ -10,18 +10,22 @@ usersRouter.get('/', async (request, response) => {
 
 usersRouter.post('/', async (request, response) => {
   const { username, password, name } = request.body
-  const saltRounds = 10
-  try {
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-    const user = new User({
-      username: username,
-      passwordHash: passwordHash,
-      name: name
-    })
-    const savedUser = await user.save()
-    response.status(201).json(savedUser)
-  } catch (error) {
-    response.status(400).end()
+  if (password.length < 3) {
+    response.status(400).json(`Password validation failed: password: Path \`password\` (\`${password}\`) is shorter than the minimum allowed length (3).`)
+  } else {
+    try {
+      const saltRounds = 10
+      const passwordHash = await bcrypt.hash(password, saltRounds)
+      const user = new User({
+        username: username,
+        passwordHash: passwordHash,
+        name: name
+      })
+      const savedUser = await user.save()
+      response.status(201).json(savedUser)
+    } catch (error) {
+      response.status(400).json(error.message)
+    }
   }
 })
 
